2a35074d8e519f1603f3f1c792eccc6a9cce270f
Galshamir8 <galshamir8@gmail.com>
2022-11-21 20:08:33 +0200

    Add EncryptedSP

--- app/src/main/java/com/example/encryptedsharedpreference/common/EncryptedSP.java
+++ app/src/main/java/com/example/encryptedsharedpreference/common/EncryptedSP.java
@@ -1,18 +1,36 @@
 package com.example.encryptedsharedpreference.common;
 
-import static com.example.encryptedsharedpreference.common.Constants.SHARED_PREFERENCE_FILE_NAME;
-
 import android.content.Context;
-import android.content.SharedPreferences;
+import android.util.Log;
 
-public class EncryptedSP {
-    private SharedPreferences sp;
+import androidx.security.crypto.EncryptedSharedPreferences;
+import androidx.security.crypto.MasterKey;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class EncryptedSP extends BasicSharedPreference{
 
     public EncryptedSP(Context ctx) {
-        sp = ctx.getSharedPreferences(SHARED_PREFERENCE_FILE_NAME, Context.MODE_PRIVATE);;
+        super(ctx);
+        try {
+            MasterKey masterKey = buildMasterKey(ctx);
+            super.sp = EncryptedSharedPreferences.create(
+                    ctx,
+                    "secret_shared_prefs",
+                    masterKey,
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            );
+        }catch (GeneralSecurityException | IOException e) {
+            Log.e("error", e.getMessage());
+        }
+
     }
 
-    public boolean getBooleanKey(String key, boolean defaultValue){
-        return sp.getBoolean(key, defaultValue);
+    private MasterKey buildMasterKey(Context ctx) throws GeneralSecurityException, IOException {
+        return new MasterKey.Builder(ctx)
+                .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+                .build();
     }
 }
