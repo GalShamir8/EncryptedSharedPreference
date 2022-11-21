4ce83f1f486ba7d071ca62aa093f7a3e4f5388b0
Galshamir8 <galshamir8@gmail.com>
2022-11-21 20:08:47 +0200

    Add new key for user details

--- app/src/main/java/com/example/encryptedsharedpreference/common/Constants.java
+++ app/src/main/java/com/example/encryptedsharedpreference/common/Constants.java
@@ -2,4 +2,6 @@ package com.example.encryptedsharedpreference.common;
 
 public interface Constants {
     String SHARED_PREFERENCE_FILE_NAME = "encryptedFile";
+    String NAME_KEY = "name";
+    String USER_DETAILS = "user_deatails";
 }
